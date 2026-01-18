d93e13137566c191531b4ef8287b164b8dd7aab9
Szilard <ivacsony.szilard@gmail.com>
2026-01-18 13:10:37 +0200

    feat(ledger): implement double-entry schema with Flyway

---
+++ ledger-service/src/main/java/com/openfashion/ledgerservice/LedgerServiceApplication.java
@@ -0,0 +1,14 @@
+package com.openfashion.ledgerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("java:S1118")
+@SpringBootApplication
+public class LedgerServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LedgerServiceApplication.class, args);
+    }
+
+}
