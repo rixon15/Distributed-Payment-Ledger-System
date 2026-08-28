41c6d4b54655d0f0d72491bd566d5a975f492228
Szilard <ivacsony.szilard@gmail.com>
2026-08-28 18:26:40 +0300

    Proto contracts defined

---
+++ grpc-contracts/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.payment.system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
